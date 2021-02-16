8e4cd50d155059da5e0df06a726bd87aea1497e5
derReloader <62664722+derReloader@users.noreply.github.com>
2021-02-16 16:57:52 +0100

    Second commit. Added documentation

--- HelloWorld/src/HelloWorld_Main.java
+++ HelloWorld/src/HelloWorld_Main.java
@@ -4,7 +4,8 @@ public class HelloWorld_Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String text = "Moin Moin";
-		System.out.println(text);		
+		System.out.println(text);	
+		// CHANGE
 	}
 
 }
