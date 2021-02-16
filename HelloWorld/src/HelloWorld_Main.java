69066140cf9565f2d4295cbebd956cc370cefd82
derReloader <62664722+derReloader@users.noreply.github.com>
2021-02-16 16:55:12 +0100

    First Commit

---
+++ HelloWorld/src/HelloWorld_Main.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text = "Moin Moin";
+		System.out.println(text);		
+	}
+
+}
